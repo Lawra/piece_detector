4fbab692c1637c2117226f8d7d64619b93496e34
lferre <laura.ferre.7@gmail.com>
2021-03-01 20:32:54 +0100

    get color

--- piece_detector.py
+++ piece_detector.py
@@ -25,8 +25,8 @@ gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh = cv2.threshold(blurred, 120, 255, cv2.THRESH_BINARY_INV)[1]
 
-# cv2.imshow('thresh', thresh)
-# cv2.waitKey(0)
+cv2.imshow('thresh', thresh)
+cv2.waitKey(0)
 
 # find contours in the thresholded image and initialize the
 # shape detector
--- utils/color_detector.py
+++ utils/color_detector.py
@@ -4,14 +4,27 @@ import imutils
 from PIL import Image
 from scipy.spatial import KDTree
 from webcolors import hex_to_rgb
+from collections import defaultdict
+import operator
 
 
 class ColorDetector:
     available_colors_list = [
-        ("#FF0000", "red"),
-        ("#008000", "green"),
-        ("#FFFF00", "yellow"),
-        ("#808080", "grey")
+        ((0, 0, 0), "black"),
+        ((255, 255, 255), "white"),
+        ((255, 0, 0), "red"),
+        ((0, 128, 0), "green"),
+        ((255, 255, 0), "yellow"),
+        ((0, 255, 0), "lime"),
+        ((0, 255, 255), "cian"),
+        ((255, 0, 255), "magenta"),
+        ((192, 192, 192), "silver"),
+        ((128, 128, 128), "grey"),
+        ((128, 0, 0), "maroon"),
+        ((128, 128, 0), "olive"),
+        ((128, 0, 128), "purple"),
+        ((0, 128, 128), "teal"),
+        ((0, 0, 128), "navy")
     ]
 
     def __init__(self):
@@ -31,26 +44,29 @@ class ColorDetector:
         cv2.waitKey(0)
 
         # get most frequent color, except black
+        resized = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(resized, 'RGB')
+        # img.show()
         w, h = img.size
         pixels = img.getcolors(w * h)
 
-        most_frequent_pixel = pixels[0]
-        if (most_frequent_pixel[1] == black):
-            most_frequent_pixel = pixels[1]
+        most_frequent_color = defaultdict(int)
 
         for count, colour in pixels:
-            if colour != black and count > most_frequent_pixel[0]:
-                most_frequent_pixel = (count, colour)
+            if colour != black:  # avoid mask
+                color_name = ColorDetector.convert_rgb_to_names(colour)
+                most_frequent_color[color_name] += count
 
-        return ColorDetector.convert_rgb_to_names(most_frequent_pixel[1])
+        print(most_frequent_color)
+        color = max(most_frequent_color.items(), key=operator.itemgetter(1))[0]
+        return color
 
     def convert_rgb_to_names(rgb_tuple):
         names = []
         rgb_values = []
-        for color_hex, color_name in ColorDetector.available_colors_list:
+        for color_rgb, color_name in ColorDetector.available_colors_list:
             names.append(color_name)
-            rgb_values.append(hex_to_rgb(color_hex))
+            rgb_values.append(color_rgb)
 
         kdt_db = KDTree(rgb_values)
         distance, index = kdt_db.query(rgb_tuple)
